4f65bb4b88cca0da859fb5b19d096f8177967db2
Mmgg Gg <ggmmgg709@gmail.com>
2025-12-17 14:02:21 +0000

    Copy ခလုတိတွေကလည်း တစ်ခါတစ်လေကြ နိပ်မရဘူး ဝယ်ယူမည် ဆိုတဲ့ခလုပ်ကလည်းနိပ်လ

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,20 +1,19 @@
 "use client"
 
-import { useToast } from "@/hooks/use-toast"
 import {
   Toast,
   ToastClose,
   ToastDescription,
-  ToastProvider,
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
+import { useToast } from "@/hooks/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <ToastProvider>
+    <>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
@@ -30,6 +29,6 @@ export function Toaster() {
         )
       })}
       <ToastViewport />
-    </ToastProvider>
+    </>
   )
 }
