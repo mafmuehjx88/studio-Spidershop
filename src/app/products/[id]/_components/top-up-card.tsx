3e0b2215c27be7f229048e8d35bd6c67c03db856
Mmgg Gg <ggmmgg709@gmail.com>
2025-12-17 05:21:44 +0000

    Iteam တွေက အပုံတွေကို 1:1 ပဲထားပေး

--- src/app/products/[id]/_components/top-up-card.tsx
+++ src/app/products/[id]/_components/top-up-card.tsx
@@ -20,7 +20,7 @@ export function TopUpCard({ option }: TopUpCardProps) {
           {option.promoTag}
         </Badge>
       )}
-      <div className="flex-grow pt-4">
+      <div className="flex-grow">
         <Image
           src={option.image.imageUrl}
           alt={option.image.description}
@@ -30,7 +30,7 @@ export function TopUpCard({ option }: TopUpCardProps) {
           data-ai-hint={option.image.imageHint}
         />
       </div>
-      <div className="flex flex-col justify-center h-14">
+      <div className="flex flex-col justify-center">
         <p className="text-sm font-semibold">{option.name}</p>
         <p className="text-xs text-muted-foreground">{option.price} ကျပ်</p>
       </div>
