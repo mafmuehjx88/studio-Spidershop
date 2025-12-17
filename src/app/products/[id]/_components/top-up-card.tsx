2a5b8f617ea9cc68422ac2021ab8283220d3d580
Mmgg Gg <ggmmgg709@gmail.com>
2025-12-17 05:51:05 +0000

    Iteam စာမျက်နှာက မလိုတဲ့စာတွေကို​ဖြုတ်ပေး

--- src/app/products/[id]/_components/top-up-card.tsx
+++ src/app/products/[id]/_components/top-up-card.tsx
@@ -10,7 +10,7 @@ export function TopUpCard({ option }: TopUpCardProps) {
   return (
     <div className="relative flex flex-col cursor-pointer group bg-[#2d2d2d] rounded-2xl p-3 text-center text-white transition-all hover:ring-2 hover:ring-green-500">
       {option.inStockTag && (
-        <div className="absolute top-0 right-0 bg-green-500 text-white text-[10px] font-bold px-3 py-1 rounded-tr-2xl rounded-bl-2xl z-10">
+        <div className="absolute top-2 right-2 badge-instock text-white text-[10px] font-bold px-2 py-0.5 rounded-md z-10">
           {option.inStockTag}
         </div>
       )}
@@ -33,10 +33,10 @@ export function TopUpCard({ option }: TopUpCardProps) {
       )}
 
 
-      <div className="relative w-full aspect-square mb-2 pt-5 flex items-center justify-center">
+      <div className="relative w-full aspect-square mb-2 flex items-center justify-center">
         <Image
           src={option.image.imageUrl}
-          alt={option.image.description}
+          alt={option.name}
           width={80}
           height={80}
           className="object-contain"
@@ -44,11 +44,11 @@ export function TopUpCard({ option }: TopUpCardProps) {
         />
       </div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col items-center">
         <p className="text-sm font-medium h-10 flex items-center justify-center">
           {option.name}
         </p>
-        <p className="text-sm font-semibold text-gray-300">{option.price}</p>
+        <p className="text-xs font-semibold text-gray-300">{option.price}</p>
       </div>
     </div>
   );
