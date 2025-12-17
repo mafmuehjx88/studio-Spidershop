5effe61f19a6a8d599c5295741f13afb555ab311
Mmgg Gg <ggmmgg709@gmail.com>
2025-12-17 15:15:34 +0000

    MLBB Philippines ရယ်
    MLBB Singapore
    
    MLBB Malaysia
    
    MLBB Indonesia
    
    ရယ

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -39,40 +39,4 @@ export const products: Product[] = [
     price: '',
     image: findImage('pubg-mobile'),
   },
-  {
-    id: 'prod_3',
-    name: 'MLBB Singapore',
-    description: 'Mobile Legends Singapore Server',
-    price: '',
-    image: findImage('mlbb-singapore'),
-    tag: 'Singapore',
-    tagColor: 'bg-red-600'
-  },
-  {
-    id: 'prod_4',
-    name: 'MLBB Philippines',
-    description: 'Mobile Legends Philippines Server',
-    price: '',
-    image: findImage('mlbb-philippines'),
-    tag: 'Philipine',
-    tagColor: 'bg-blue-800'
-  },
-  {
-    id: 'prod_5',
-    name: 'MLBB Indonesia',
-    description: 'Mobile Legends Indonesia Server',
-    price: '',
-    image: findImage('mlbb-indonesia'),
-    tag: 'Indonesia',
-    tagColor: 'bg-red-700'
-  },
-  {
-    id: 'prod_6',
-    name: 'MLBB Malaysia',
-    description: 'Mobile Legends Malaysia Server',
-    price: '',
-    image: findImage('mlbb-malaysia'),
-    tag: 'Malaysia',
-    tagColor: 'bg-yellow-500'
-  },
 ];
