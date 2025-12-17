bc394e6e0d9ad0a78a2a874a2db9ca3626d5f3e7
Mmgg Gg <ggmmgg709@gmail.com>
2025-12-17 04:36:37 +0000

    ဂိမ်းကိုတစ်တန်းကို 3ခုထားပေးပါဆို ကျစ်ကျစ်လစ်လစ်လေး တစ်ချားဟာတွေကိုရောပဲ

--- src/components/home/product-grid.tsx
+++ src/components/home/product-grid.tsx
@@ -5,16 +5,16 @@ import { Marquee } from "@/components/ui/marquee";
 export function ProductGrid() {
   return (
     <div className="bg-background">
-      <section className="container mx-auto px-4 py-12 md:px-6 md:py-16">
+      <section className="container mx-auto px-4 py-8 md:px-6 md:py-12">
         <div className="mb-8 overflow-hidden whitespace-nowrap">
            <Marquee>
               အသုံးပြုကြတဲ့ Users အားလုံး စိတ်ကျေနပ်မှုအပြည့်အဝရရှိစေရန်
            </Marquee>
         </div>
-        <h2 className="mb-8 text-center font-headline text-3xl font-bold uppercase tracking-wider text-primary-foreground">
+        <h2 className="mb-8 text-center font-headline text-3xl font-bold uppercase tracking-wider text-primary">
           Our Products
         </h2>
-        <div className="grid grid-cols-2 gap-x-4 gap-y-8 md:grid-cols-3 md:gap-x-6 md:gap-y-10">
+        <div className="grid grid-cols-3 gap-x-4 gap-y-8 md:grid-cols-3 md:gap-x-6 md:gap-y-10">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
