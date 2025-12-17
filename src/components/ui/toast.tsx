ba1e697244a76ecb7b4a425f052582f41a5ecc1a
Mmgg Gg <ggmmgg709@gmail.com>
2025-12-17 14:11:07 +0000

    အပေါ်က ကျလာတဲ့စာကို ဖောက်မြင်နေရတဲ့ပုံဆန်မလုပ်ပဲ နောက်ခံအနက်ရောင်ပေါ်မှာ

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -31,9 +31,9 @@ const toastVariants = cva(
       variant: {
         default: "border bg-background text-foreground",
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "destructive group border-red-500 bg-red-600 text-white",
         success:
-          "success group border-green-500 bg-green-500/10 text-green-300",
+          "success group border-green-500 bg-black text-white",
       },
     },
     defaultVariants: {
@@ -98,7 +98,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
+      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600 group-[.success]:text-gray-300 group-[.success]:hover:text-white",
       className
     )}
     toast-close=""
