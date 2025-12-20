006c6b3d714dd670b78e86de42b5911038006252
Mmgg Gg <ggmmgg709@gmail.com>
2025-12-20 14:35:32 +0000

    home scree က OUR PRODUCTS ရဲ့အပေါ် ပြေးနေတဲ့စာကြောင်လေးထည့်ပေး

--- src/components/home/product-grid.tsx
+++ src/components/home/product-grid.tsx
@@ -7,9 +7,16 @@ export function ProductGrid() {
   return (
     <div className="bg-background">
       <section className="container mx-auto px-4 py-8 md:px-6 md:py-12">
-        <h2 className="mb-8 text-center font-headline text-2xl font-bold uppercase tracking-normal text-primary">
-          OUR PRODUCTS
-        </h2>
+        <div className="mb-8 space-y-4">
+            <div className="bg-card p-2 rounded-md">
+                <Marquee>
+                    Spider Game Shop မှကြိုဆိုပါသည်
+                </Marquee>
+            </div>
+          <h2 className="text-center font-headline text-2xl font-bold uppercase tracking-normal text-primary">
+            OUR PRODUCTS
+          </h2>
+        </div>
         <div className="grid grid-cols-3 gap-x-4 gap-y-8 md:grid-cols-3 md:gap-x-6 md:gap-y-10">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
